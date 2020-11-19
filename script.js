258c1034a611f9257fb85a93582f6ae2bc5b5f01
Your name here <louise.thomsson@netrelations.com>
2020-11-19 15:35:02 +0100

    new
    new

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var img1 = document.getElementById(".blog-section_image"),
+img2 = document.getElementById(".blog-section_image");
+
+img1.onmouseover = function(){
+img2.style.display = "block";
+}
+
+img1.onmouseout = function(){
+img2.style.display = "none";
+}
